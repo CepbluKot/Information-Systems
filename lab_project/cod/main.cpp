3a657f27fdc1cc73e6f36381c61aaf16da376896
CepbluKot <igorcat100@gmail.com>
2022-05-22 09:45:58 +0300

    add camera interface

--- lab_project/cod/main.cpp
+++ lab_project/cod/main.cpp
@@ -1,20 +1,21 @@
 #include "opencv4/opencv2/opencv.hpp"
-
-
+#include "camera.h"
 using namespace cv;
 
 
 int main(int, char **)
 {
-  VideoCapture cap(0); // Открываем камеру или файл
-  if (!cap.isOpened()) // check if we succeeded
-    return -1;
+  Camera* cam1 = new Camera;
+  cameraInterpretation cameraClient(cam1);
+  
+  cameraClient.openCamera();
+
   Mat edges;
   namedWindow("edges", 1);
   while (1)
   {
-    Mat frame;
-    cap >> frame;                                     // get a new frame from camera
+    Mat frame = cameraClient.getFrame();
+                                         // get a new frame from camera
     cvtColor(frame, edges, COLOR_BGR2GRAY);           // Перевод в градации серого
     GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5); // Размытие
     Canny(edges, edges, 0, 30, 3);                    // Выделение границ
